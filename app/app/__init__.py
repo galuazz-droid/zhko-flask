2c718e4a9a16f68b1859f093f0311dd947cdac32
galuazz-droid <galuazz@gmail.com>
2025-09-29 01:26:58 +0300

    Create __init__.py

---
+++ app/app/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('config.Config')  # или пропишите конфигурацию здесь
+    db.init_app(app)
+    
+    from app.routes import main_bp
+    app.register_blueprint(main_bp)
+    
+    return app
